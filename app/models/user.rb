641536eaed0c8c30bb79dabf563d67617c2f5fac
takumi555 <htaaa838317@gmail.com>
2020-11-04 14:27:59 +0900

    loginページとsigninページのcss修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
