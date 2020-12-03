f1b5a8082390ea7ec4fc52c1d018ac539da8bea9
takumi555 <htaaa838317@gmail.com>
2020-12-03 19:12:24 +0900

    relationshipsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,20 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
+  end
 
   def hasliked?(post)
     likes.exists?(post_id: post.id)
