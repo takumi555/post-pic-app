e091553ce66f604198b19548a23a41dde6e56cbd
takumi555 <htaaa838317@gmail.com>
2020-12-20 16:55:17 +0900

    @user名でコメントしたときに、そのユーザーにメールで通知する機能を設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,17 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+document.addEventListener("turbolinks:load"
+, function () {
+    $(function () {
+      $('.anime').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+    });
+  })
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
